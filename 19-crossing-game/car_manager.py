3f743aba277bb0d4eb37f5deb117b2ebdd997a7a
ramatov-zulfikor <ramatovv@gmail.com>
2021-06-15 13:05:48 +0500

    day-19 source-code

---
+++ 19-crossing-game/car_manager.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.move_distance = 5
+
+    def create_car(self):
+        chance = random.randint(1, 6)
+        if chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(self.move_distance)
+
+    def increase_move_distance(self):
+        self.move_distance += 1
---
+++ 19-crossing-game/main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+player = Player()
+screen.listen()
+screen.onkeypress(player.move_forward, "Up")
+
+car_manager = CarManager()
+
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    if player.is_on_finish():
+        player.start_again()
+        car_manager.increase_move_distance()
+        scoreboard.increase_level()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
