1792d7d7cf14caba85cd78bd92b896a9ad72aba0
ramatov-zulfikor <ramatovv@gmail.com>
2021-06-15 12:59:48 +0500

    day-01 source-code

---
+++ 01-band-name-generator/main.py
@@ -0,0 +1,6 @@
+print("Hello! Welcome to the Band Name Generator.")
+print("What's name of the city you grew up?")
+city = input()
+print("What's your pet's name?")
+pets = input()
+print("Your band name could be", city, pets)
