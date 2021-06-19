51c0462c5b6c7de923e152b2dbcc8f5038faff3e
ramatov-zulfikor <ramatovv@gmail.com>
2021-06-19 13:36:33 +0500

    improved password-manager with try-except

--- 24-password-manager/main.py
+++ 24-password-manager/main.py
@@ -2,10 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, shuffle
 import pyperclip
+import json
 
 
-def generate():
-    """Generate password"""
+def generate_password():
+    """ Generate password """
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -23,26 +24,47 @@ def generate():
     pyperclip.copy(password_input.get())
 
 
-def save():
-    """Save password to data.txt"""
-    user_website = website_input.get()
+def save_password():
+    """ Save password to data.json """
+    user_website = website_input.get().lower()
     user_email = email_input.get()
     user_password = password_input.get()
+
+    user_data = {
+        user_website: {
+            "email": user_email,
+            "password": user_password
+        }
+    }
 
     if len(user_website) == 0 or len(user_email) == 0 or len(user_password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=user_website, message=f"These are the details entered: "
-                                                                   f"\nEmail: {user_email} "
-                                                                   f"\nPassword: {user_password} "
-                                                                   f"\nIs it ok to save?")
-
-        if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{user_website} | {user_email} | {user_password}\n")
-
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(user_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(user_data, data_file, indent=4)
+        else:
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
+
+
+def search_password():
+    """ Search password from data.json """
+    website = website_input.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            result = data[website.lower()]
+            messagebox.showinfo(title=website, message=f"Email: {result['email']}\n Password: {result['password']}")
+    except KeyError:
+        messagebox.showinfo(title="Oops", message="No details for the website exists")
 
 
 # UI
@@ -66,23 +88,26 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Inputs
-website_input = Entry(width=49)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=30)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 email_input = Entry(width=49)
 email_input.grid(row=2, column=1, columnspan=2)
 # Here you can uncomment line under and set your email by default
-# email_input.insert(0, "example@email.com")
+email_input.insert(0, "example@email.com")
 
 password_input = Entry(width=30)
 password_input.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text="Generate Password", command=generate)
+search_button = Button(text="Search", width=14, command=search_password)
+search_button.grid(row=1, column=2)
+
+generate_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=43, command=save)
+add_button = Button(text="Add", width=43, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
